9bb42582c386ceb5b16a38158fd823554b4ab2c0
Tim <TimCPB@github.com>
2020-06-11 15:54:33 +0100

    created interface.js containing jquery commands

--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,61 +1,65 @@
-function Thermostat() {
-  this.temperature = 20;
-  this.MINIMUM_TEMP = 10;
-  this.powerSavingMode = true;
-  this.PS_ON_MAX = 25;
-  this.PS_OFF_MAX = 32;
-  this.LOW_USAGE = 18;
+class Thermostat {
+  constructor() {
+    this.temperature = 20;
+    this.MINIMUM_TEMP = 10;
+    this.powerSavingMode = true;
+    this.PS_ON_MAX = 25;
+    this.PS_OFF_MAX = 32;
+    this.LOW_USAGE = 18;
 
-};
-
-Thermostat.prototype.currentTemp = function() {
-  return this.temperature;
-};
-
-Thermostat.prototype.up = function() {
-  if (this.isMaxTemp()) {
-    return;
   }
-  return this.temperature ++;
-};
-
-Thermostat.prototype.down = function() {
-  if (this.isMinimumTemp()) {
-    return;
+  currentTemp() {
+    return this.temperature;
   }
-  return this.temperature --;
-};
-
-Thermostat.prototype.isMinimumTemp = function() {
-  return this.temperature === this.MINIMUM_TEMP;
-};
-
-Thermostat.prototype.isPowerSaverOn = function() {
-  return this.powerSavingMode === true;
-};
-Thermostat.prototype.turnPowerSaverOff = function () {
-  return this.powerSavingMode = false;
-};
-Thermostat.prototype.turnPowerSaverOn = function () {
-  return this.powerSavingMode = true;
-};
-Thermostat.prototype.isMaxTemp = function() {
-  if (this.isPowerSaverOn() === false) {
-    return this.temperature === this.PS_OFF_MAX;
+  up() {
+    if (this.isMaxTemp()) {
+      return;
+    }
+    return this.temperature++;
   }
-  return this.temperature === this.PS_ON_MAX;
+  down() {
+    if (this.isMinimumTemp()) {
+      return;
+    }
+    return this.temperature--;
+  }
+  isMinimumTemp() {
+    return this.temperature === this.MINIMUM_TEMP;
+  }
+  isPowerSaverOn() {
+    return this.powerSavingMode === true;
+  }
+  turnPowerSaverOff() {
+    return this.powerSavingMode = false;
+  }
+  turnPowerSaverOn() {
+    return this.powerSavingMode = true;
+  }
+  isMaxTemp() {
+    if (this.isPowerSaverOn() === false) {
+      return this.temperature === this.PS_OFF_MAX;
+    }
+    return this.temperature === this.PS_ON_MAX;
+  }
+  reset() {
+    return this.temperature = 20;
+  }
+  energyUsage() {
+    if (this.currentTemp() <= this.LOW_USAGE) {
+      return "Low usage";
+    }
+    else if (this.currentTemp() <= this.PS_ON_MAX) {
+      return "Medium usage";
+    }
+    return "High usage";
+  }
 }
+;
 
-Thermostat.prototype.reset = function() {
-  return this.temperature = 20;
-};
 
-Thermostat.prototype.energyUsage =  function() {
-  if (this.currentTemp() <= this.LOW_USAGE) {
-    return "Low usage";
-  }
-  else if (this.currentTemp() <= this.PS_ON_MAX) {
-    return "Medium usage";
-  }
-  return "High usage"
-};
+
+
+
+
+
+
