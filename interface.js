fe0f71c81946670f64d70439c86ce21080164106
Tim <TimCPB@github.com>
2020-06-12 11:10:02 +0100

    Added current weather API and displaying current temperature

--- interface.js
+++ interface.js
@@ -21,8 +21,11 @@ $(document).ready(function() {
   $('#powersaving-off').on('click', function(){
     thermostat.turnPowerSaverOff();
     $('#power-saving-status').text('off')
-    updateTemperature();
+    updateTemperature();  
   })
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=c2d102055ae8d191cb13bfd37d12059d&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
 
   function updateTemperature() {
     $('#Temperature').text(thermostat.temperature);
